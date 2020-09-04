bde584b6ed3a9da952e0c9c9a963e81825d7b573
Vuong Tran <vuongtran.cg@gmail.com>
2020-09-04 22:19:29 +0700

    Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,72 @@
+import React from "react";
 import Head from "next/head";
 import {
   Container,
-  Row,
-  Col,
-  Button,
   Card,
   CardText,
   CardTitle,
   CardBody,
+  CardImg,
+  CardImgOverlay,
 } from "reactstrap";
+import FsLightbox from "fslightbox-react";
 
+function Collections({ data }) {
+  const [toggler, setToggler] = React.useState(false);
+  const [sources, setSources] = React.useState([]);
+
+  async function handleOnClick(collectionId) {
+    const res = await fetch(
+      `https://api.unsplash.com//collections/${collectionId}/photos?client_id=${CLIENT_ID}`
+    );
+    const data = await res.json();
+    console.log("data", data);
+    let s = [];
+    data.map((i) => {
+      s.push(i.urls.full);
+    });
+    console.log("s", s);
+    setSources(s);
+    setToggler(!toggler);
+  }
+
+  return (
+    <>
+      <div className="card-columns">
+        {data &&
+          data.map((collection) => {
+            return (
+              <Card
+                className="card__noborder"
+                inverse
+                key={collection.id}
+                onClick={() => handleOnClick(collection.id)}
+              >
+                <CardImg
+                  width="100%"
+                  src={collection.cover_photo.urls.small}
+                  alt={collection.cover_photo.alt_description}
+                />
+                <CardImgOverlay>
+                  <CardTitle>{`${collection.title} (${collection.total_photos})`}</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            );
+          })}
+      </div>
+      {sources.length > 0 && (
+        <FsLightbox
+          toggler={toggler}
+          sources={sources}
+          onClose={() => {
+            setSources([]);
+          }}
+        />
+      )}
+    </>
+  );
+}
 export default function Home({ data }) {
-  console.log("data", data);
-
   return (
     <Container className="md-container">
       <Head>
@@ -20,79 +74,23 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <Container>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p>
+        <h1 className="title">{`Animal For Kid <3`}</h1>
+        {/* <p>
           Get started by editing <code>pages/index.js</code>
-        </p>
-        <Container>
-          <Row>
-            <Col sm="4">
-              <Card className="my-3">
-                <CardBody>
-                  <CardTitle tag="h5">Documentation</CardTitle>
-                  <CardText>
-                    Find in-depth information about Next.js features and API.
-                  </CardText>
-                  <Button color="primary" href="https://nextjs.org/docs">
-                    More &rarr;
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col sm="4">
-              <Card className="my-3">
-                <CardBody>
-                  <CardTitle tag="h5">Learn</CardTitle>
-                  <CardText>
-                    Learn about Next.js in an interactive course with quizzes!
-                  </CardText>
-                  <Button color="primary" href="https://nextjs.org/learn">
-                    More &rarr;
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col sm="4">
-              <Card className="my-3">
-                <CardBody>
-                  <CardTitle tag="h5">Documentation</CardTitle>
-                  <CardText>
-                    Find in-depth information about Next.js features and API.
-                  </CardText>
-                  <Button color="primary" href="https://nextjs.org/docs">
-                    More &rarr;
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col sm="4">
-              <Card className="my-3">
-                <CardBody>
-                  <CardTitle tag="h5">Learn</CardTitle>
-                  <CardText>
-                    Learn about Next.js in an interactive course with quizzes!
-                  </CardText>
-                  <Button color="primary" href="https://nextjs.org/learn">
-                    More &rarr;
-                  </Button>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+        </p> */}
+        <Collections data={data} />
       </Container>
 
       <footer className="cntr-footer">
-        <a
+        @2020 Build width Nextjs | Image by Unsplash | Host by Vercel
+        {/* <a
           href="https://vercel.com?filter=next.js&utm_source=github&utm_medium=example&utm_campaign=next-example"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="sml-logo" />
-        </a>
+        </a> */}
       </footer>
     </Container>
   );
@@ -105,10 +103,7 @@ export async function getServerSideProps() {
     `https://api.unsplash.com/users/vuongtran/collections?client_id=${CLIENT_ID}`
   );
   const data = await res.json();
-  const res1 = await fetch(
-    `https://api.unsplash.com//collections/11104369/photos?client_id=${CLIENT_ID}`
-  );
-  console.log(await res1.json());
+
   // Pass data to the page via props
   return { props: { data } };
 }
